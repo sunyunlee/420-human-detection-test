54456be147178427c9cf09d73ab3b32768e5665a
Sunyun Lee <sunyunlee@Sunyuns-MacBook-Pro.local>
2020-12-15 16:28:28 -0500

    updated HOG detector

--- sd_detect.py
+++ sd_detect.py
@@ -1,8 +1,9 @@
 import numpy as np
-from typing import Union 
+from typing import Union, Tuple, List
 import cv2
 from imutils.object_detection import non_max_suppression
 import imutils
+import matplotlib.pyplot as plt
 
 
 weight_path = "yolov3.weights"
@@ -106,7 +107,7 @@ def process_yolov3_output(outputs, input_shape):
     return boxes_filtered
 
 
-def HOG(img, scale: int=0.5):
+def HOG(img, win_stride: Tuple[int], padding: Tuple[int], scale: int):
     """
     Returns bounding boxes around humans that the HOG detectors detect
     * x1 is the person's bounding box's left
@@ -114,23 +115,83 @@ def HOG(img, scale: int=0.5):
     * x2 is the person's bounding box's right
     * y2 is the person's bounding box's bottom 
     """
-    
     hog = cv2.HOGDescriptor()
+    # hog = cv2.HOGDescriptor(img.shape, (8, 8), (4, 4), (8, 8), 9, 1, -1, 0, 0.2, 1, 64, True)
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-    scale = 0.5
-
     img = imutils.resize(img, width=int(scale*img.shape[1]))
-    rects, weights = hog.detectMultiScale(img, winStride=(2, 2), padding=(10, 10), scale=scale)
+    hog_params = {'winStride': win_stride, 'padding': padding, 'scale': 1.05}
 
-    # pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    rects, weights = hog.detectMultiScale(img, **hog_params)
+
+    # pick = non_max_suppression(rects, probs=None, overlapThresh=0.1)
     results = []
     for i, (x, y, w, h) in enumerate(rects): 
-        if weights[i][0] > 0.2:
-            x1 = int(x / scale)
-            y1 = int(y / scale)
-            x2 = int((x + w) / scale)
-            y2 = int((y + h) / scale)
-            results.append([x1, y1, x2, y2])
+        x1 = int(x / scale)
+        y1 = int(y / scale)
+        x2 = int((x + w) / scale)
+        y2 = int((y + h) / scale)
+        results.append([x1, y1, x2, y2])
 
-    return results
+    return results, list(weights.flatten())
+
+
+def remove_overlapping_boxes(boxes: List[List[int]]) -> List[List[int]]:
+    """ Removes bounding boxes which completely overlaps another bounding box
+
+    :param boxes: the bounding boxes
+    :return: the filtered bounding boxes 
+    """
+    box_to_remove = []
+    for i in range(len(boxes)):
+        curr_box = boxes[i]
+        for j in range(len(boxes)):
+            if i != j: 
+                new_box = boxes[j]
+                if curr_box[0] <= new_box[0] and \
+                    curr_box[1] <= new_box[1] and \
+                        curr_box[2] >= new_box[2] and \
+                            curr_box[3] >= new_box[3]:
+                    box_to_remove.append(i)
+
+    box_to_remove = sorted(list(set(box_to_remove)))
+
+    for i in range(len(box_to_remove) - 1, -1, -1):
+        boxes.pop(box_to_remove[i])
+
+    return boxes
+                
+
+def HOG_remove_low_confidence(boxes: List[List[int]], weights: List[int], confid_thresh: int = 1):
+    """ Returns bounding boxes and the associated weights with weights lower than 
+    the confidence threshold 
+
+    :param boxes: the bounding boxes 
+    :param weights: the confidences/weights of the predictions
+    :param confid_thresh: the confidence threshold
+    :return: list of bounding boxes and the associated weights 
+    """
+    indices = []
+    for i in range(len(weights)):
+        if weights[i] < confid_thresh: 
+            indices.append(i)
+    
+    boxes_filtered = remove_indices(boxes, indices)
+    weights_filtered = remove_indices(weights, indices)
+
+    return boxes_filtered, weights_filtered
+
+
+def remove_indices(l: list, indices: List[int]) -> list:
+    """ Given indices and a list, the function returns a list with the items at indices removed
+
+    :param l: the list of items
+    :param indices: list of indices at which to remove the items
+    :return: list l with items removed 
+    """
+    for i in range(len(indices) - 1, -1, -1):
+        if indices[i] >= len(l):
+            raise Exception("Index out of bounds!")
+        l.pop(indices[i])
+    
+    return l

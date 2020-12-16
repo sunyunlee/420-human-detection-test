de13820869a12570c42d060d827739e82c142c93
AlexHeyman <abheyman23@sbcglobal.net>
2020-12-16 14:04:59 -0500

    Allowed sd_measure.py to run without errors and made it produce temporary output

--- sd_measure.py
+++ sd_measure.py
@@ -16,38 +16,42 @@ def measure_locations(image, bboxes, roi, scale):
 
     This function returns a list of dicts. Each dict represents a person in the
     image, and has the following key-value pairs:
-    * bbox: Represents the person's bounding box in the image. A tuple of four
+    * 'bbox': Represents the person's bounding box in the image. A tuple of four
     integers, formatted like in the output of sd_detect.detect_people().
-    * location: Represents the location of the person's center in 3D space. A
-    tuple of three numbers (X, Y, Z), with Z as the vertical coordinate. Scale
-    is in meters.
-    * too_close: A list containing the indices in the master list of the other
+    * 'location': Represents the location of the person's center in 3D space. A
+    numpy array of three numbers [X, Y, Z], with Z as the vertical coordinate.
+    Scale is in meters.
+    * 'too_close': A list containing the indices in the master list of the other
     people to whom this person is closer than two meters.
     """
 
     transform = cv2.getPerspectiveTransform(
-        roi, ((1, 0), (1, 1), (0, 1), (0, 0)))
-    tfed_scale = cv2.perspectiveTransform(scale, transform)
+        np.float32(roi), np.float32(((1, 0), (1, 1), (0, 1), (0, 0))))
+    tfed_scale = cv2.perspectiveTransform(np.float32((scale,)), transform)[0]
     two_meters = np.linalg.norm(tfed_scale[1] - tfed_scale[0])
     two_meters_v = np.linalg.norm(tfed_scale[2] - tfed_scale[0])
     ratio = two_meters_v / two_meters
-    
+
     people = []
     for bbox in bboxes:
         #For now, we're treating everyone's center as the 3D point corresponding
         #to their 2D bounding box's bottom center
-        center_2d = (bbox[1] + bbox[0])/2
-        tfed_center_2d, = cv2.perspectiveTransform((center2d,), transform)
-        center_3d = (tfed_center_2d[1]/ratio, tfed_center_2d[0], 0)
-        people.append({bbox: bbox, location: center3d, too_close: []})
+        center_2d = ((bbox[1] + bbox[3])/2, bbox[0])
+        tfed_center_2d = cv2.perspectiveTransform(np.float32(((center_2d,),)),
+                                                  transform)[0, 0]
+        center_3d = np.array((tfed_center_2d[1]/ratio, tfed_center_2d[0], 0))
+        people.append({'bbox': bbox, 'location': center_3d, 'too_close': []})
+
+    for i1 in range(len(people)):
+        p1 = people[i1]
+        for i2 in range(i1):
+            p2 = people[i2]
+            dist = np.linalg.norm(p2['location'] - p1['location'])
+            if dist < two_meters:
+                p1['too_close'].append(i2)
+                p2['too_close'].append(i1)
 
     for i in range(len(people)):
-        p1 = people[i]
-        for j in range(i):
-            p2 = people[j]
-            dist = np.linalg.norm(p2.location - p1.location)
-            if dist < two_meters:
-                p1.too_close.append(p2)
-                p2.too_close.append(p1)
+        print(str(i) + ': ' + str(people[i]))
     
     return people
